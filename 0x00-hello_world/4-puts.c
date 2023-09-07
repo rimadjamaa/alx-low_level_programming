d2a21df03f5363d4c13894c826b33caf39bd540c
rimadjamaa <djamaarima7@gmail.com>
2023-09-07 15:54:42 -0700

    atsk 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,11 @@
 
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+/**
+  *it will print the text
+  *without using printf
+  *puts syntax
+  */
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
