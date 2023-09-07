fb97ec771d25980163d7da6d8b5d6e4391213dc1
rimadjamaa <djamaarima7@gmail.com>
2023-09-07 15:56:55 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 int main(void)
 {
 /**
-  *it will print the text
-  *without using printf
-  *puts syntax
-  */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
