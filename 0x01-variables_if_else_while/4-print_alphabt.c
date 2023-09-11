55e7013782957b5c4e3e7001cb33553128673c87
rimadjamaa <djamaarima7@gmail.com>
2023-09-11 05:16:20 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,10 +9,13 @@ int main(void)
 char letters = 'a';
 while (letters <= 'z')
 {
+if (letters == 'q' || letters == 'e')
+letters++;
 putchar(letters);
 letters++;
 }
 putchar('\n');
 return (0);
 }
+
 
