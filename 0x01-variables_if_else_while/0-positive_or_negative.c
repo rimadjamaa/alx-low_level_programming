8e6f113cf98e309d73a49867e3846fe41db2d9ff
rimadjamaa <djamaarima7@gmail.com>
2023-09-11 03:22:39 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
- * this main fonction will check
- * if the number is pos or neg
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -12,12 +13,12 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/*we use if to check*/
-	if(n>0)
-	  printf("%d is positive \n",n);
-	else if(n==0)
-	  printf("%d is zero \n",n);
+	if ( n > 0 )
+	  printf("%d is positive\n", n);
+	else if ( n == 0 )
+	  printf("%d is zero\n", n);
 	else
-	  printf("%d is negative \n ",n);
+	  printf("%d is negative\n", n);
 	return (0);
 }
 
