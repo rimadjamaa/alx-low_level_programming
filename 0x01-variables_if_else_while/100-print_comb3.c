ad7d3ead8d673104e17b1c40d5e3f7e32344c652
rimadjamaa <djamaarima7@gmail.com>
2023-09-11 06:59:36 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int d1, d2;
+
+for (d1 = '0'; d1 < '9'; d1++)
+{
+for (d2 = d1 + 1; d2 <= '9'; d2++)
+{
+putchar(d1);
+putchar(d2);
+if (d1 != '8' ||  d2 != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+
+putchar('\n');
+return (0);
+}
