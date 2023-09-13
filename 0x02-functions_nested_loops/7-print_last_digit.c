9d95dc1100f43af8df5f2f7dc3dba0f5cb530bf5
rimadjamaa <djamaarima7@gmail.com>
2023-09-13 06:20:44 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_last_digit:- 
+*@num: the number
+* Return: the last digit
+*/
+int print_last_digit(int num)
+{
+int num=num % 10;
+return (num);
+}
+
