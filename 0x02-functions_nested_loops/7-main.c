7a17ff243794814b7238d249c671a5f6fc941ccc
rimadjamaa <djamaarima7@gmail.com>
2023-09-13 07:01:49 -0700

    task 7

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int r;
 
-	r = print_last_digit(98);
+	r = print_last_digit(-1024);
 	_putchar('0' + r);
 	_putchar('\n');
 	return (0);
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,7 +7,10 @@
 */
 int print_last_digit(int num)
 {
-int lastdigit = num % 10;
+int lastdigit;
+if (num < 0)
+num = -num;
+lastdigit = num % 10;
 _putchar('0' + lastdigit);
 return (lastdigit);
 }
