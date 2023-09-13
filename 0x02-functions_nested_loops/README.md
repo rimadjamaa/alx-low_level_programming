542738eb20e5868a18654d3d15e5a8682e6e34ff
rimadjamaa <djamaarima7@gmail.com>
2023-09-13 00:31:34 -0700

    task 0

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+in this directory we gonna make the tasks about nested loop and header files 
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+/* Add prototypes for other functions if needed */
+
+#endif /* MAIN_H */
