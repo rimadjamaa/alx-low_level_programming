f3f4c93d782c4255f59d5c0bd90af67c236f5ce5
rimadjamaa <djamaarima7@gmail.com>
2023-09-14 03:42:39 -0700

    task 0

--- 0x03-debugging/1-main.C
+++ 0x03-debugging/1-main.C
@@ -7,20 +7,21 @@
 
 int main(void)
 {
-int i;
+    int i;
 
-printf("Infinite loop incoming :(\n");
+    printf("Infinite loop incoming :(\n");
 
-i = 0;
+    i = 0;
 
-/**
- *while (i < 10)
- *{
- *putchar(i);
- *}
-*/
+    // Comment out the infinite loop below
+    /*
+    while (i < 10)
+    {
+        printf("%d", i);  // Print the value of i as an integer
+    }
+    */
 
-printf("Infinite loop avoided! \\o/\n");
+    printf("Infinite loop avoided! \\o/\n");
 
-return (0);
+    return (0);
 }
