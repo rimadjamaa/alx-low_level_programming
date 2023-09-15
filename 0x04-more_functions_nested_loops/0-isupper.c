80a415042265786bd312a8321f026f9b41178de7
rimadjamaa <djamaarima7@gmail.com>
2023-09-15 04:05:23 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,7 @@
 /**
  * _isupper - checks for uppercase character
  * @c: The character to be checked
+ *
  * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -10,9 +10,9 @@ int main(void)
 {
     char c;
 
-    c = 'A';
+    c = 'Z';
     printf("%c: %d\n", c, _isupper(c));
-    c = 'a';
+    c = 'd';
     printf("%c: %d\n", c, _isupper(c));
     return (0);
 }
