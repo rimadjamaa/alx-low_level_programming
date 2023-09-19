680e51cb2f3c91ff0aca5d421ccfac89c22a02ab
rimadjamaa <djamaarima7@gmail.com>
2023-09-19 08:02:47 -0700

    task 3

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@ int _putchar(char c);/* Function to print a character */
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
 /* Add prototypes for other functions if needed */
 
 #endif /* MAIN_H */
