4cf37b49bae1577e1ac0a2056dc7dda1a9ee28d1
rimadjamaa <rimadjamaa7@gmail.com>
2023-09-21 12:32:00 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include "main.h"
 /**
  * *_stract - swap between 2 int
  * @dest: destination file
  * @src: source file
- * Return: file destination wich contain the content of source file 
-*/
+ * Return: file destination wich contain the content of source file
+ */
 char *_strcat(char *dest, char *src)
 {
 char *result = dest;
