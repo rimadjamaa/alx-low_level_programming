7624056aeee1f35db51aa495241eea7355ae75ba
rimadjamaa <rimadjamaa7@gmail.com>
2023-09-21 12:44:49 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+
 /**
  * *strncat - swap between 2 int
+ *
  * @dest: first pointer
  * @src: second pointer
- * @n :
+ * @n: number of bytes to copy
+ *
  * Return: nothing
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
 char *result = dest;
