68fd5c885af5bcdabded1df85f1fe855880e04c4
rimadjamaa <rimadjamaa7@gmail.com>
2023-09-22 13:10:19 -0700

    task 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,7 +10,7 @@ int _atoi(char *s)
 int result = 0;
 int sign = 1;
 int i = 0;
-while (s[i] == ' ' || s[i] == '-' || s[i] == '+')
+while (s[i] && (s[i] < '0' || s[i] > '9'))
 {
 if (s[i] == '-')
 {
