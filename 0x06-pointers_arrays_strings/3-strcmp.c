d9b36f7e50dae46dc66fdb3392358d0a1bd593a9
rimadjamaa <rimadjamaa7@gmail.com>
2023-09-22 05:03:31 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,27 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * _strcmp- this function will switch between tow strings
- * @s1: dest file
- * @s2: source file
- * Return: the string
- */
-int _strcmp(const char *s1, const char *s2)
+* _strcmp - Compare two strings
+* @s1: First string
+* @s2: Second string
+*
+* Return: 0 if the strings are equal, a negative value if s1 is less than s2,
+*         or a positive value if s1 is greater than s2.
+*/
+int _strcmp(char *s1, char *s2)
 {
-while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
+while (*s1 != '\0' && *s2 != '\0')
 {
+if (*s1 != *s2)
+return (*s1 - *s2);
 s1++;
 s2++;
 }
-if (*s1 < *s2)
-{
-return (-15);
-}
-else if (*s1 > *s2)
-{
-return (15);
-}
-else
-{
-return (0);
-}
+return (*s1 - *s2);
 }
