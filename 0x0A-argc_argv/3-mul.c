8e4e6d795ab16f5a570f70b195cecd6eef2a09cf
rimadjamaa <rimadjamaa7@gmail.com>
2023-10-05 07:24:42 -0700

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,16 +10,16 @@
  */
 int main(int argc, char *argv[])
 {
+int num1, num2;
 if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
 
-int num1 = atoi(argv[1]);
-int num2 = atoi(argv[2]);
-
-printf("%d\n", num1 * num2);
+printf("%d\n", num1 *num2);
 
 return (0);
 }
