fa4b6b1c0f810093f13995f417d5fe40cb978b6d
rimadjamaa <rimadjamaa7@gmail.com>
2023-10-05 07:02:59 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+/**
+ * main - Entry point of the program
+ * @argc: Number of arguments passed to the program
+ * @argv: Array of strings containing the arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+return (0);
+}
