9bff189ed2f7e5e9a087676f409404862cdf4a80
rimadjamaa <rimadjamaa7@gmail.com>
2023-10-09 12:59:27 -0700

    task 0

---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+this repo will contain task about malloc also 
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+void *malloc_checked(unsigned int b);
+/* Add prototypes for other functions if needed */
+
+#endif /* MAIN_H */
