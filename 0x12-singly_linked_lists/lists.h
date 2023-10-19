fb0752684400fa3efaab697e9326cbdfc99b3486
rimadjamaa <rimadjamaa7@gmail.com>
2023-10-19 14:58:26 -0700

    task 4

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -25,4 +25,5 @@ list_t *add_node(list_t **head, const char *str);
 unsigned int _strlen(char *str);
 list_t *add_node_end(list_t **head, const char *str);
 unsigned int _strlen(char *str);
+void free_list(list_t *head);
 #endif /* LISTS_H */
