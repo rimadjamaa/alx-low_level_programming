7e8616c0414ca7b4bedc32fcc02e87c8ac645d6d
rimadjamaa <rimadjamaa7@gmail.com>
2023-10-19 14:50:32 -0700

    fixing errors

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,7 @@
 #include "lists.h"
-
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 /**
  * add_node - Adds a new node at the beginning of a list_t list.
  * @head: Pointer to the head of the list
