c0b79e200500f04a02d610520f9f0b4924e4c905
rimadjamaa <rimadjamaa7@gmail.com>
2023-10-23 09:20:26 -0700

    task 3

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -16,9 +16,9 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-/* Function prototypes */
 size_t print_listint(const listint_t *h);
 int _putchar(char c);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 #endif /* LISTS_H */
