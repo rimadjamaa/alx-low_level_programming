ff51c7cb9ddd5bb71af1f41c0b7c1ac9f151496d
rimadjamaa <rimadjamaa7@gmail.com>
2023-11-01 01:51:23 -0700

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,19 +11,21 @@
  */
 int main(int argc, char *argv[])
 {
-int fd_from, fd_to, n_read, n_write, close;
+int fd_from, fd_to, c_f, c_t, n_read;
 char buf[BUF_SIZE];
 if (argc != 3)
 {
 dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
 exit(97);
 }
-if ((fd_from = open(argv[1], O_RDONLY) == -1)
+fd_from = open(argv[1], O_RDONLY);
+if (fd_from == -1)
 {
 dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-if ((fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664) == -1)
+fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (fd_to == -1)
 {
 dprintf(2, "Error: Can't write to %s\n", argv[2]);
 exit(99);
@@ -41,9 +43,11 @@ if (n_read == -1)
 dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-if ((c == close(fd_from)) == -1 || (c == close(fd_to)) == -1)
+c_f = close(fd_from);
+c_t = close(fd_to);
+if (c_f == -1 || c_t == -1)
 {
-dprintf(2, "Error: Can't close fd %d\n", c  == -1 ? fd_from : fd_to);
+dprintf(2, "Error: Can't close fd %d\n", c_f ? fd_from : fd_to);
 exit(100);
 }
 return (0);
